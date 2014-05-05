a18eaa7fa9638e42ada438d0a13b9d280337590c
el1ven <tangGPA@gmail.com>
2014-05-05 16:07:13 +0800

    Update ViewController.m
    添加了一些注释

--- testSQL/ViewController.m
+++ testSQL/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+//利用宏定义常量，方便以后使用
 #define DBNAME @"personinfo.sqlite"
 #define NAME @"name"
 #define AGE @"age"
@@ -26,13 +27,13 @@
     
 	NSString *path = [[NSHomeDirectory() stringByAppendingString:@"/Documents/"]stringByAppendingPathComponent:DBNAME];//设置数据库的路径
 
-    if(sqlite3_open([path UTF8String], &db)!=SQLITE_OK){
+    if(sqlite3_open([path UTF8String], &db)!=SQLITE_OK){//打开数据库，没有就根据路径创建
         sqlite3_close(db);
         NSLog(@"数据库打开失败!");
     }
     
     /*NSString *sqlCreateTable = @"CREATE TABLE IF NOT EXISTS PERSONINFO (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER, address TEXT )";
-    [self execSql:sqlCreateTable];
+    [self execSql:sqlCreateTable];//创建table
     
     NSString *sql1 = [NSString stringWithFormat:
                       @"INSERT INTO '%@' ('%@', '%@', '%@') VALUES ('%@', '%@', '%@')",
@@ -42,13 +43,13 @@
                       @"INSERT INTO '%@' ('%@', '%@', '%@') VALUES ('%@', '%@', '%@')",
                       TABLENAME, NAME, AGE, ADDRESS, @"老六", @"20", @"东城区"];
     
-    [self execSql:sql1];
+    [self execSql:sql1];//插入数据库
     [self execSql:sql2];
      */
      
     
     NSString *sqlQuery = @"select * from PERSONINFO";
-    sqlite3_stmt *statement;
+    sqlite3_stmt *statement;//查询数据库
     
     if(sqlite3_prepare(db, [sqlQuery UTF8String], -1, &statement, nil) == SQLITE_OK){
         
@@ -62,14 +63,14 @@
             char *address = (char*)sqlite3_column_text(statement, 3);
             NSString *nsAddressStr = [[NSString alloc]initWithUTF8String:address];
             
-            NSLog(@"name:%@ age:%d address:%@",nsNameStr,age,nsAddressStr);
+            NSLog(@"name:%@ age:%d address:%@",nsNameStr,age,nsAddressStr);//在控制台打印出来
             
             
         }
         
     }
     
-    sqlite3_close(db);
+    sqlite3_close(db);//记得关闭数据库
     
     
     
@@ -79,7 +80,7 @@
     
 }
 
--(void)execSql:(NSString *)sql
+-(void)execSql:(NSString *)sql//可以执行任何sql语句的函数
 {
     char *err;
     if (sqlite3_exec(db, [sql UTF8String], nil, nil, &err) != SQLITE_OK) {
